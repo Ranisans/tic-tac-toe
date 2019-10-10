a205dc51c907e2c864d9fcf4ae957514c421c95f
Ranisans <ranisans@yahoo.com>
2019-10-10 12:38:12 +0300

    feat: add task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,73 @@
 class TicTacToe {
-    constructor() {
-
+  constructor() {
+    this.board = new Array(3);
+    for (let i = 0; i < this.board.length; i++) {
+      this.board[i] = new Array(3).fill(null);
     }
+    this.currentPlayerSymbol = { true: "x", false: "o" };
+    this.isCross = true;
+    this.winner;
+    this.winPositions = [
+      [[[0], [0]], [[0], [1]], [[0], [2]]],
+      [[[1], [0]], [[1], [1]], [[1], [2]]],
+      [[[2], [0]], [[2], [1]], [[2], [2]]],
+      [[[0], [0]], [[1], [0]], [[2], [0]]],
+      [[[0], [1]], [[1], [1]], [[2], [1]]],
+      [[[0], [2]], [[1], [2]], [[2], [2]]],
+      [[[0], [0]], [[1], [1]], [[2], [2]]],
+      [[[2], [0]], [[1], [1]], [[0], [2]]]
+    ];
+  }
 
-    getCurrentPlayerSymbol() {
+  getCurrentPlayerSymbol() {
+    return this.currentPlayerSymbol[this.isCross];
+  }
 
+  nextTurn(rowIndex, columnIndex) {
+    if (this.board[rowIndex][columnIndex] === null) {
+      this.board[rowIndex][columnIndex] = this.currentPlayerSymbol[
+        this.isCross
+      ];
+      this.isCross = !this.isCross;
     }
+  }
 
-    nextTurn(rowIndex, columnIndex) {
+  isFinished() {
+    if (!this.getWinner() && !this.noMoreTurns()) return false;
+    return true;
+  }
 
+  getWinner() {
+    for (let i = 0; i < this.winPositions.length; i++) {
+      const winPosition = this.winPositions[i];
+      if (
+        this.board[winPosition[0][0]][winPosition[0][1]] ===
+          this.board[winPosition[1][0]][winPosition[1][1]] &&
+        this.board[winPosition[0][0]][winPosition[0][1]] ===
+          this.board[winPosition[2][0]][winPosition[2][1]] &&
+        this.board[winPosition[0][0]][winPosition[0][1]] !== null
+      )
+        return this.board[winPosition[0][0]][winPosition[0][1]];
     }
+    return null;
+  }
 
-    isFinished() {
-
+  noMoreTurns() {
+    for (let row of this.board) {
+      if (row.includes(null)) return false;
     }
+    return true;
+  }
 
-    getWinner() {
+  isDraw() {
+    if (this.getWinner() || !this.noMoreTurns()) return false;
+    return true;
+  }
 
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    const field = this.board[rowIndex][colIndex];
+    return field ? field : null;
+  }
 }
 
 module.exports = TicTacToe;
